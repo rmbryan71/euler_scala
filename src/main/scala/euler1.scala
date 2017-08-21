8c6f4c21a602ebf5c4c7d7ae9ab4fed69819c6f8
Rob Bryan <rob@rmbryan.com>
2017-08-21 08:08:36 -0400

    Corrected upper limit of loop. Answer returned by this code is confirmed correct by projecteuler.net Keeping "unnecessary parentheses"

--- src/main/scala/euler1.scala
+++ src/main/scala/euler1.scala
@@ -1,4 +1,8 @@
+/*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.*/
+
 object euler1 extends App{
-  def answer = (0 to 1000).filter((a) => (a % 3 == 0 || a % 5 == 0)).sum
+  def answer = (0 to 999).filter((a) => (a % 3 == 0 || a % 5 == 0)).sum
   println(answer)
 }
